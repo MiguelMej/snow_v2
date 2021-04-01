a00dd3502987a4b9cb8b6b173fca18ab2d15a778
Sebmolendijk <sebmolendijk@gmail.com>
2021-04-01 18:06:24 +0200

    Update CustomMapping.js

--- scriptIncludes/CustomMapping.js
+++ scriptIncludes/CustomMapping.js
@@ -6,34 +6,40 @@ CustomMapping.prototype = {
         var entitiesUtils = new Entities();
         var incidentTable = gs.getProperty('x_556309_microsoft.incidentTableName');
         var myObj = new GlideRecord(incidentTable);
+        var appUtils = new AppUtils();
         // Add your specific mappings below, using the incident entities and alerts
 
-        var ips = entitiesUtils.getEntitiesByType(incidentEntities, 'ip');
-        var hosts = entitiesUtils.getEntitiesByType(incidentEntities, 'host');
-        var users = entitiesUtils.getEntitiesByType(incidentEntities, 'account');
+        try {
+            var ips = entitiesUtils.getEntitiesByType(incidentEntities, 'ip');
+            var hosts = entitiesUtils.getEntitiesByType(incidentEntities, 'host');
+            var users = entitiesUtils.getEntitiesByType(incidentEntities, 'account');
 
-        if(ips) {
-            myObj.u_ips = (ips.map(function (ip) {return ip.details.address;})).join(', ');
-            appUtils.log(myObj.u_ips);
+            if(ips) {
+                myObj.u_ips = (ips.map(function (ip) {return ip.details.address;})).join(', ');
+                appUtils.log(myObj.u_ips);
+            }
+            
+            if(hosts) {
+                myObj.u_hosts = (hosts.map(function (host) {return host.details.hostName;})).join(', ');
+                appUtils.log(myObj.u_hosts);
+            }
+            
+            if(users) {
+                myObj.u_impacted_users = (users.map(function (user) {return user.details.accountName;})).join(', ');
+                appUtils.log(myObj.u_impacted_users);
+            }
+
+            myObj.u_other_case_number = incident.properties.incidentNumber;
+            myObj.attack_vector = incident.properties.additionalData.tactics.join(', ');
+            myObj.u_alert_source = incident.properties.additionalData.alertProductNames.join(', ');
+            myObj.contact_type = 'SIEM';
+
+            myObj.update();
+
         }
-        
-        if(hosts) {
-            myObj.u_hosts = (hosts.map(function (host) {return host.details.hostName;})).join(', ');
-            appUtils.log(myObj.u_hosts);
+        catch (err) {
+            appUtils.log('ERROR CustomMapping: ' + err );
         }
-        
-        if(users) {
-            myObj.u_impacted_users = (users.map(function (user) {return user.details.accountName;})).join(', ');
-            appUtils.log(myObj.u_impacted_users);
-        }
-
-        myObj.u_other_case_number = incident.property.incidentNumber;
-        myObj.attack_vector = incident.property.additionalData.tactics.toString();
-        myObj.u_alert_source = incident.property.additionalData.alertProductNames;
-        myObj.contact_type = 'SIEM';
-
-        myObj.update();
-
 
     },
     type: 'CustomMapping'
