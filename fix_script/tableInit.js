3a7f4c7e76177e3efe8f7a32e254a0cfc5d83011
Sebmolendijk <sebmolendijk@gmail.com>
2021-04-27 17:32:44 +0200

    fixed tables init script

--- fix_script/tableInit.js
+++ fix_script/tableInit.js
@@ -22,45 +22,105 @@ var workspaceConfig = new GlideRecord("x_556309_microsoft_workspaces_config");
 workspaceConfig.name = 'Company Sentinel Workspace';
 workspaceConfig.insert();
 
-// Initialize x_556309_microsoft_sentinel_incident_to_servicenow
-var severities = ['Informal', 'Low', 'Medium', 'High'];
-var sentinelSeverity = new GlideRecord("x_556309_microsoft_sentinel_incident_to_servicenow");
+// Initialize "Sentinel Severity to ServiceNow" table(x_556309_microsoft_sentinel_incident_to_servicenow)
+var severities = ['Informational', 'Low', 'Medium', 'High'];
+var sentinelSeverityRecord = new GlideRecord("x_556309_microsoft_sentinel_incident_to_servicenow");
 
 for(var i=0; i < severities.length; i++) {
-    sentinelSeverity.newRecord();
-    sentinelSeverity.sentinel_severity = severities[i];
+    sentinelSeverityRecord.initialize();
+    sentinelSeverityRecord.sentinel_severity = severities[i];
+
     switch (severities[i]) {
-        case 'High': sentinelSeverity.servicenow_severity = 1;
+        case 'High': sentinelSeverityRecord.servicenow_severity = '1';
             break;
-        case 'Medium': sentinelSeverity.servicenow_severity = 2;
+        case 'Medium': sentinelSeverityRecord.servicenow_severity = '2';
             break;
-        case 'Low': sentinelSeverity.servicenow_severity = 3;
+        case 'Low': sentinelSeverityRecord.servicenow_severity = '3';
             break;
-        default: sentinelSeverity.servicenow_severity = 3;
+        case 'Informational': sentinelSeverityRecord.servicenow_severity = '3';
             break;
     }
-    sentinelSeverity.insert();
+    sentinelSeverityRecord.insert();
 }
 
 
-// Initialize x_556309_microsoft_servicenow_incident_to_sentinel
-var servicenowSeverity = new GlideRecord('x_556309_microsoft_servicenow_incident_to_sentinel');
+// Initialize "ServiceNow Severity to Sentinel" table (x_556309_microsoft_servicenow_incident_to_sentinel)
+var snowSeveritieRecord = new GlideRecord('x_556309_microsoft_servicenow_incident_to_sentinel');
+var snowSeverities = ['1', '2', '3'];
 
-for(var i = 1; i <= 3; i++) {
-    servicenowSeverity.newRecord();
-    servicenowSeverity.servicenow_severity = i;
+for(var i=0; i < snowSeverities.length; i++) {
 
-    switch (servicenowSeverity.servicenow_severity) {
-        case 1: servicenowSeverity.sentinel_severity = 'High';
+    snowSeveritieRecord.initialize();
+    snowSeveritieRecord.servicenow_severity = snowSeverities[i];
+
+    switch (snowSeverities[i]) {
+        case '1': snowSeveritieRecord.sentinel_severity = 'High';
             break;
-        case 2: servicenowSeverity.sentinel_severity = 'Medium';
+        case '2': snowSeveritieRecord.sentinel_severity = 'Medium';
             break;
-        case 3: servicenowSeverity.sentinel_severity = 'Low';
-            break;
-        default: servicenowSeverity.sentinel_severity = 'Low';
+        case '3': snowSeveritieRecord.sentinel_severity = 'Low';
             break;
     }
-    servicenowSeverity.insert();
+    snowSeveritieRecord.insert();
 
 }
+
+// Initialize "Sentinel State to ServiceNow" table (x_556309_microsoft_sentinel_state_to_servicenow)
+var sentinelStates = ['New', 'Active', 'Closed'];
+var sentinelStatesRecord = new GlideRecord("x_556309_microsoft_sentinel_state_to_servicenow");
+
+for(var i=0; i < sentinelStates.length; i++) {
+    sentinelStatesRecord.initialize();
+    sentinelStatesRecord.sentinel_state = sentinelStates[i];
+
+    switch (sentinelStates[i]) {
+        case 'New': sentinelStatesRecord.servicenow_state = '10';
+            break;
+        case 'Active': sentinelStatesRecord.servicenow_state = '16';
+            break;
+        case 'Closed': sentinelStatesRecord.servicenow_state = '3';
+            break;
+    }
+    sentinelStatesRecord.insert();
+}
+
+
+// Initialize "ServiceNow State to Sentinel" table (x_556309_microsoft_sentinel_state_to_servicenow)
+var snowStates = ['1', '2', '3', '6', '7', '8', '10', '16', '18', '19', '20', '100'];
+var snowStatesRecord = new GlideRecord("x_556309_microsoft_servicenow_state_to_sentinel");
+
+for(var i=0; i < snowStates.length; i++) {
+    snowStatesRecord.initialize();
+    snowStatesRecord.servicenow_state = snowStates[i];
+
+    switch (snowStates[i]) {
+        case '1': snowStatesRecord.sentinel_state = 'New';
+            break;
+        case '2': snowStatesRecord.sentinel_state = 'Active';
+            break;
+        case '3': snowStatesRecord.sentinel_state = 'Closed';
+            break;
+        case '6': snowStatesRecord.sentinel_state = 'Closed';
+            break;
+        case '7': snowStatesRecord.sentinel_state = 'Closed';
+            break;
+        case '8': snowStatesRecord.sentinel_state = 'Closed';
+            break;
+        case '10': snowStatesRecord.sentinel_state = 'New';
+            break;
+        case '16': snowStatesRecord.sentinel_state = 'Active';
+            break;
+        case '18': snowStatesRecord.sentinel_state = 'Active';
+            break;
+        case '19': snowStatesRecord.sentinel_state = 'Active';
+            break;
+        case '20': snowStatesRecord.sentinel_state = 'Active';
+            break;
+        case '100': snowStatesRecord.sentinel_state = 'Active';
+            break;
+    }
+    snowStatesRecord.insert();
+}
+
+
 
