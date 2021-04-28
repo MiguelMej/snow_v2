b05e54a8d518bc29a0708abd3f39c54b53d69dda
Sebmolendijk <sebmolendijk@gmail.com>
2021-04-28 18:37:38 +0200

    Removed references to old table sysutils

--- fix_script/tableInit.js
+++ fix_script/tableInit.js
@@ -4,19 +4,6 @@ var date = new Date();
 date.setDate(date.getDate() - 30);
 var lastSync = date.toISOString();
 
-
-var utils = new GlideRecord("x_556309_microsoft_systemutils");
-utils.newRecord();
-utils.property = 'newIncidentsLastSync';
-utils.value = lastSync;
-utils.insert();
-
-utils = new GlideRecord("x_556309_microsoft_systemutils");
-utils.newRecord();
-utils.property = 'modifiedIncidentsLastSync';
-utils.value = lastSync;
-utils.insert();
-
 // Initialize the first Azure Sentinel workspace configuration
 var workspaceConfig = new GlideRecord("x_556309_microsoft_workspaces_config");
 workspaceConfig.name = 'Company Sentinel Workspace';
